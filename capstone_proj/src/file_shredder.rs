d187d02e010c80d1435b99fa56bf0b13e6d280d8
damiankryzia71 <79245828+damiankryzia71@users.noreply.github.com>
2025-04-14 21:01:26 -0700

    Update file_shredder.rs

--- capstone_proj/src/file_shredder.rs
+++ capstone_proj/src/file_shredder.rs
@@ -7,12 +7,12 @@ use rand::RngCore;
 fn shred_file<P: AsRef<Path>>(path: P, passes: u8) -> io::Result<()> {
     let path = path.as_ref();
 
-    let mut file = OpenOptions::new().read(true).write(true).open(path)?;
+    let mut file = OpenOptions::new().read(true).write(true).open(path);
 
-    let file_len = file.metadata()?.len();
+    let file_len = file.metadata().len();
 
     for _ in 0..passes {
-        file.seek(SeekFrom::Start(0)?);
+        file.seek(SeekFrom::Start(0));
         let mut buffer = vec![0u8; 1024 * 1024];
 
         let mut bytes_remaining = file_len;
@@ -20,18 +20,18 @@ fn shred_file<P: AsRef<Path>>(path: P, passes: u8) -> io::Result<()> {
             let write_size = std::cmp::min(buffer.len() as u64, bytes_remaining) as usize;
 
             OsRng.fill_bytes(&mut buffer[..write_size]);
-            file.write_all(&buffer[..write_size])?;
+            file.write_all(&buffer[..write_size]);
 
             bytes_remaining -= write_size as u64;
         }
 
-        file.flush()?;
+        file.flush();
     }
 
-    file.sync_all()?;
+    file.sync_all();
     drop(file);
 
-    remove_file(path)?;
+    remove_file(path);
 
     Ok(())
 }
